14bb42206d52cd67c1bf6b0caf57108c0dc232ac
EthanLien1 <ethanlien2008@gmail.com>
2023-12-15 11:29:32 -0800

    main

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 # This file was created by: Ethan Lien
-# Project title: Trouble
 
-# Goals: IQ game that measures the user's IQ
-# Create a escape room
-
-# I am passionate about playing games because their are so many 
-# different types of controls and pathways to go down to. 
-# I can utilize this passion to code because I can use my previous knowledge of games to modify or even 
-# build a better game and modify last game
         
